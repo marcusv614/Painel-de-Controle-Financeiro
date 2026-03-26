d0c9eb9046ca1d9aee2670afc1cdb4057173d083
Desktop Marcus <marcus.pinheiro@super.ufam.edu.br>
2026-03-25 21:23:54 -0400

    feat: criar entidade Account

---
+++ src/main/java/br/com/marcus/painel_financeiro/model/Account.java
@@ -0,0 +1,47 @@
+package br.com.marcus.painel_financeiro.model;
+
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="tb_accounts")
+public class Account {
+    
+    @Id
+    @GeneratedValue(strategy=GenerationType.UUID)
+    @Column(name="account_id")
+    private UUID accountId;
+
+    @Column(name="description")
+    private String description;
+
+    public Account() {
+    }
+
+    public Account(UUID accountId, String description) {
+        this.accountId = accountId;
+        this.description = description;
+    }
+
+    public UUID getAccountId() {
+        return accountId;
+    }
+
+    public void setAccountId(UUID accountId) {
+        this.accountId = accountId;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
